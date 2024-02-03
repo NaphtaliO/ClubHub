46186d45f55a432b97cc064069ebba22e1e4da77
Naphtali <naphtali2003@gmail.com>
2024-02-03 16:52:14 +0000

    Added firebase in the project

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,6 +5,42 @@ import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { Provider } from 'react-redux';
 import store from './src/redux/store'
 
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import {
+  APIKEY,
+  AUTHDOMAIN,
+  PROJECTID,
+  STORAGEBUCKET,
+  MESSAGINGSENDERID,
+  APPID,
+  MEASUREMENTID,
+} from "@env";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID,
+  measurementId: MEASUREMENTID,
+};
+
+let app;
+// Initialize Firebase
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+}
+
+const storage = getStorage(app);
+
 export default function App() {
   return (
     <Provider store={store}>
--- frontend/src/types/env.d.ts
+++ frontend/src/types/env.d.ts
@@ -1,4 +1,11 @@
 declare module '@env' {
+    export const APIKEY: string;
+    export const AUTHDOMAIN: string;
+    export const PROJECTID: string;
+    export const STORAGEBUCKET: string;
+    export const MESSAGINGSENDERID: string;
+    export const APPID: string;
+    export const MEASUREMENTID: string;
     export const URL: string;
     export const VERSION: string;
 }
